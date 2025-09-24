7e03b8ac2639d7ded63b71116622e5a5254fca20
Weldon <weldonbarros179@gmail.com>
2025-09-24 08:18:51 -0300

    feat(api): aprimorar integração da API de filmes

--- src/app/features/movies/api/movie.api.ts
+++ src/app/features/movies/api/movie.api.ts
@@ -1,21 +1,26 @@
 import { Injectable, inject } from '@angular/core';
 import { Observable } from 'rxjs';
 import { MovieResponse } from '../types/movie.type';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class MovieApiService {
   private http = inject(HttpClient);
-  private readonly apiKey = process.env["NG_APP_API_KEY"];
-  private readonly apiUrl = 'https://api.themoviedb.org/3';
+  private readonly apiKey = environment.apiKey;
+  private readonly apiUrl = environment.apiUrl;
 
   getPopularMovies(page = 1): Observable<MovieResponse> {
-    console.log(this.apiKey)
-    return this.http.gt<MovieResponse>(`${this.apiUrl}/movie/popular?api_key=${this.apiKey}&page=${page}`);
+    return this.http.get<MovieResponse>(
+      `${this.apiUrl}/movie/popular?api_key=${this.apiKey}&page=${page}`
+    );
   }
 
   searchMovies(query: string, page = 1): Observable<MovieResponse> {
-    return this.http.gt<MovieResponse>(`${this.apiUrl}/search/movie?api_key=${this.apiKey}&query=${query}&page=${page}`);
+    return this.http.get<MovieResponse>(
+      `${this.apiUrl}/search/movie?api_key=${this.apiKey}&query=${query}&page=${page}`
+    );
   }
 }
