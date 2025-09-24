b747673dee7d2604d25774f6ce83ec424b9b5db8
Weldon <weldonbarros179@gmail.com>
2025-09-24 08:18:43 -0300

    feat(app): ajustar layout e lógica do componente principal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,33 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ReactiveFormsModule, FormControl } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { MovieFacade } from './features/movies/services/movie.facade';
+import { debounceTime, distinctUntilChanged } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ReactiveFormsModule, CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'Filmes';
+  searchControl = new FormControl('');
+
+  constructor(private facade: MovieFacade) {
+    this.searchControl.valueChanges
+      .pipe(
+        debounceTime(300),
+        distinctUntilChanged()
+      )
+      .subscribe(query => {
+        if (query) {
+          this.facade.searchMovies(query);
+        } else {
+          this.facade.loadPopularMovies();
+        }
+      });
+  }
 }
