a5be41d1a6397695a48a6f32d2e3f3ebf73e8371
Weldon <weldonbarros179@gmail.com>
2025-09-24 08:19:08 -0300

    feat(movie-list): implementar ajustes visuais e lógicos na listagem de filmes

--- src/app/features/movies/pages/movie-list/movie-list.component.ts
+++ src/app/features/movies/pages/movie-list/movie-list.component.ts
@@ -5,13 +5,20 @@ import { MovieCardComponent } from '../../components/movie-card/movie-card.compo
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
 import { debounceTime, distinctUntilChanged, startWith } from 'rxjs/operators';
 import { CarouselItem } from '@shared/components/carousel/carousel.component';
+import { CarouselComponent } from '@shared/components/carousel/carousel.component';
 
 @Component({
   selector: 'app-movie-list',
   templateUrl: './movie-list.component.html',
   styleUrls: ['./movie-list.component.scss'],
   standalone: true,
-  imports: [CommonModule, AsyncPipe, MovieCardComponent, ReactiveFormsModule]
+  imports: [
+    CommonModule,
+    AsyncPipe,
+    MovieCardComponent,
+    ReactiveFormsModule,
+    CarouselComponent,
+  ],
 })
 export class MovieListComponent implements OnInit {
   facade = inject(MovieFacade);
@@ -25,17 +32,15 @@ export class MovieListComponent implements OnInit {
   ngOnInit() {
     this.loadMovieCategories();
 
-    this.searchControl.valueChanges.pipe(
-      startWith(''),
-      debounceTime(300),
-      distinctUntilChanged()
-    ).subscribe(query => {
-      if (query) {
-        this.facade.searchMovies(query);
-      } else {
-        this.facade.loadPopularMovies();
-      }
-    });
+    this.searchControl.valueChanges
+      .pipe(startWith(''), debounceTime(300), distinctUntilChanged())
+      .subscribe((query) => {
+        if (query) {
+          this.facade.searchMovies(query);
+        } else {
+          this.facade.loadPopularMovies();
+        }
+      });
   }
 
   private loadMovieCategories() {
@@ -44,15 +49,15 @@ export class MovieListComponent implements OnInit {
 
     // For demo purposes, we'll use the same data for different categories
     // In a real app, you'd have separate API calls for each category
-    this.facade.movies$.subscribe(state => {
+    this.facade.movies$.subscribe((state) => {
       if (state.movies.length > 0) {
-        const movies = state.movies.map(movie => ({
+        const movies = state.movies.map((movie) => ({
           id: movie.id,
           title: movie.title,
           imgSrc: movie.poster_path,
           link: `/movie/${movie.id}`,
           rating: (movie.vote_average / 10) * 100, // Convert to percentage for star rating
-          vote: movie.vote_average
+          vote: movie.vote_average,
         }));
 
         this.popularMovies = movies;
