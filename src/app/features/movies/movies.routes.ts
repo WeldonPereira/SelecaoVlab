b451f1b6e172ce37aaf5a04dc832036a0d3bdb81
Weldon <weldonbarros179@gmail.com>
2025-09-25 09:02:54 -0300

    feat(routes): adicionar rota para página de detalhes do filme

--- src/app/features/movies/movies.routes.ts
+++ src/app/features/movies/movies.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { MovieListComponent } from './pages/movie-list/movie-list.component';
+import { MovieDetailsComponent } from './pages/movie-detail/movie-details.component';
 
 export const MOVIE_ROUTES: Routes = [
   {
     path: '',
-    component: MovieListComponent
-  }
+    component: MovieListComponent,
+  },
+  { path: ':id', component: MovieDetailsComponent },
 ];
