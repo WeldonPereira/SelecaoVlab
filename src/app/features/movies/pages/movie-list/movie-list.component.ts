e4131f58def10cae5e00b771f5a85957367316e0
Weldon <weldonbarros179@gmail.com>
2025-09-25 13:29:19 -0300

    feat(movie-list): exibir carousels separados e integração com busca

--- src/app/features/movies/pages/movie-list/movie-list.component.ts
+++ src/app/features/movies/pages/movie-list/movie-list.component.ts
@@ -2,84 +2,78 @@ import { Component, OnInit, inject } from '@angular/core';
 import { MovieFacade } from '../../services/movie.facade';
 import { AsyncPipe, CommonModule } from '@angular/common';
 import { MovieCardComponent } from '../../components/movie-card/movie-card.component';
-import { FormControl, ReactiveFormsModule } from '@angular/forms';
-import { debounceTime, distinctUntilChanged, startWith } from 'rxjs/operators';
 import { CarouselItem } from '@shared/components/carousel/carousel.component';
 import { CarouselComponent } from '@shared/components/carousel/carousel.component';
+import { MovieStateService } from '../../state/movie.state';
 
 @Component({
   selector: 'app-movie-list',
   templateUrl: './movie-list.component.html',
   styleUrls: ['./movie-list.component.scss'],
   standalone: true,
-  imports: [
-    CommonModule,
-    AsyncPipe,
-    MovieCardComponent,
-    ReactiveFormsModule,
-    CarouselComponent,
-  ],
+  imports: [CommonModule, AsyncPipe, MovieCardComponent, CarouselComponent],
 })
 export class MovieListComponent implements OnInit {
-  facade = inject(MovieFacade);
-  searchControl = new FormControl('');
+  state = inject(MovieStateService);
+  private facade = inject(MovieFacade);
+
+  searchQuery = '';
 
   popularMovies: CarouselItem[] = [];
   topRatedMovies: CarouselItem[] = [];
   upcomingMovies: CarouselItem[] = [];
-  nowPlayingMovies: CarouselItem[] = [];
 
   ngOnInit() {
     this.loadMovieCategories();
 
-    this.searchControl.valueChanges
-      .pipe(startWith(''), debounceTime(300), distinctUntilChanged())
-      .subscribe((query) => {
-        if (query) {
-          this.facade.searchMovies(query);
-        } else {
-          this.facade.loadPopularMovies();
-        }
-      });
+    this.state.movies$.subscribe((state) => {
+      const hasSearchResults = state.movies.length > 0;
+
+      if (hasSearchResults) {
+        this.popularMovies = state.movies.map((movie) =>
+          this.mapToCarouselItem(movie)
+        );
+        this.topRatedMovies = [];
+        this.upcomingMovies = [];
+
+        this.searchQuery = this.facade.lastSearchQuery || '';
+      } else {
+        this.popularMovies = state.popularMovies.map((movie) =>
+          this.mapToCarouselItem(movie)
+        );
+        this.topRatedMovies = state.topRatedMovies.map((movie) =>
+          this.mapToCarouselItem(movie)
+        );
+        this.upcomingMovies = state.upcomingMovies.map((movie) =>
+          this.mapToCarouselItem(movie)
+        );
+        this.searchQuery = '';
+      }
+    });
   }
 
   private loadMovieCategories() {
     this.facade.loadPopularMovies();
+    this.facade.loadTopRatedMovies();
+    this.facade.loadUpcomingMovies();
+  }
 
-    this.facade.movies$.subscribe((state) => {
-      if (state.movies.length > 0) {
-        const movies = state.movies.map((movie) => ({
-          id: movie.id,
-          title: movie.title,
-          imgSrc: movie.poster_path,
-          link: `/movie/${movie.id}`,
-          rating: (movie.vote_average / 10) * 100,
-          vote: movie.vote_average,
-          releaseDate: movie.release_date,
-          duration: 0,
-        }));
+  private mapToCarouselItem(movie: any): CarouselItem {
+    const item: CarouselItem = {
+      id: movie.id,
+      title: movie.title,
+      imgSrc: movie.poster_path,
+      link: `/movie/${movie.id}`,
+      rating: (movie.vote_average / 10) * 100,
+      vote: movie.vote_average,
+      duration: 0,
+      releaseDate: movie.release_date,
+    };
 
-        this.popularMovies = movies;
-
-        movies.forEach((m) => {
-          this.facade.loadMovieDetails(m.id).subscribe((details) => {
-            m.duration = details.runtime;
-          });
-        });
-
-        this.topRatedMovies = [...movies]
-          .sort((a, b) => b.vote - a.vote)
-          .slice(0, 20);
-
-        this.upcomingMovies = [...movies]
-          .filter((movie) => new Date(movie.releaseDate) <= new Date())
-          .sort(
-            (a, b) =>
-              new Date(b.releaseDate).getTime() -
-              new Date(a.releaseDate).getTime()
-          )
-          .slice(0, 20);
-      }
+    this.facade.loadMovieDetails(item.id).subscribe((details) => {
+      item.duration = details.runtime;
     });
+
+    return item;
   }
 }
